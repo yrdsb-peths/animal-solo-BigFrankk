7fd4d56d0c873630d5a5e8556f2e72762edf7da1
BigFrankk <440044938@gapps.yrdsb.ca>
2025-05-12 08:42:25 -0400

    b

--- MyWorld.java
+++ MyWorld.java
@@ -1,12 +1,15 @@
 import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
+        //create a new world
         super(600, 400, 1);
         
+        //Create the elephant object
         Elephant elephant = new Elephant();
         addObject(elephant,300,200);
         
-        
+        //create a label
+        Lable scoreLabel = new Lable;
         createApple();
     }
     
