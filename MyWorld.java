5d1dbcb0e32ce923c61d5452e8ecb29fa020c5fe
BigFrankk <440044938@gapps.yrdsb.ca>
2025-05-13 08:52:25 -0400

    gameover

--- MyWorld.java
+++ MyWorld.java
@@ -1,20 +1,36 @@
 import greenfoot.*;
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
     public MyWorld() {
+       
         //create a new world
-        super(600, 400, 1);
+        super(600, 400, 1,false);
         
         //Create the elephant object
         Elephant elephant = new Elephant();
         addObject(elephant,300,200);
         
         //create a label
-        Label scoreLabel = new Label();
-        addObject()
+        scoreLabel = new Label(0,80);
+        addObject(scoreLabel,50,50);
         
         createApple();
     }
     
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("'Game Over",100);
+        addObject(gameOverLabel,300,200);
+    }
+    /**
+     * Increase our score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
     
     public void createApple()
     {
